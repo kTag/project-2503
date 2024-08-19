c9c737d5180b0b4a47fae798f4beb1d66c7ad7f1
kTag <ggizmo+github@me.com>
2024-08-19 18:49:04 +0800

    Loading secrets in env variables and updated get_tweet code

---
+++ init_op.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+export TWITTER_ACCESS_TOKEN="op://Personal/ltpqaj6x27ixpnkbvbcucj4rdq/access token" 
+export TWITTER_ACCESS_TOKEN_SECRET="op://Personal/ltpqaj6x27ixpnkbvbcucj4rdq/access secret"
+export TWITTER_CONSUMER_KEY="op://Personal/ltpqaj6x27ixpnkbvbcucj4rdq/client id"
+export TWITTER_CONSUMER_SECRET="op://Personal/ltpqaj6x27ixpnkbvbcucj4rdq/client secret" 
