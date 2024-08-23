8553e14afdd4312a3cc962bb136a8df34492d15e
kTag <ggizmo+github@me.com>
2024-08-23 17:39:25 +0800

    Second feed file and code updated to generate the feed with the Nostr entries

--- get_nostr_note.py
+++ get_nostr_note.py
@@ -6,6 +6,7 @@ import tornado.ioloop
 from rich.console import Console
 from rich.table import Table
 from tornado import gen
+from feedgen.feed import FeedGenerator
 
 from pynostr.base_relay import RelayPolicy
 from pynostr.event import EventKind
@@ -42,6 +43,24 @@ if __name__ == "__main__":
     event_msgs = message_pool.get_all_events()
     print(f"{r.url} returned {len(event_msgs)} TEXT_NOTEs from {input_str}.")
 
+    fg = FeedGenerator()
+    fg.id('https://snow-services.com')
+    fg.title('kTag Personal Nostr Feed')
+    fg.author( {'name':'kTag','email':'sdf@sdf.com'} )
+    fg.link( href='https://github.com/kTag/project-2503', rel='alternate' )
+    fg.logo('https://raw.githubusercontent.com/kTag/project-2503/nostr/onektag.png')
+    fg.subtitle('Nostr Feed')
+    fg.link( href='https://snow-services.com', rel='self' )
+    fg.language('en')
+
     for event_msg in event_msgs[::-1]:
         readable_event=event_msg.event.to_dict()
-        print(f"{(readable_event)}\n\n")
+        fe = fg.add_entry()
+        fe.id(f'https://snow-services.com/{readable_event['id']}')
+        fe.title(readable_event['pubkey'])
+        fe.description(readable_event['content'])
+        fe.link(href="https://snow-services.com")
+        #print(f"{(readable_event)}\n\n")
+
+    atomfeed = fg.atom_str(pretty=True) # Get the ATOM feed as string
+    fg.atom_file('atom.xml') # Write the ATOM feed to a file
