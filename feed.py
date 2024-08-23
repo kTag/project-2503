ee12f43c3b0fc05c906ef83b41aaf85891932e78
kTag <ggizmo+github@me.com>
2024-08-23 17:26:39 +0800

    Added logo and updating feed generation code

--- feed.py
+++ feed.py
@@ -1,12 +1,12 @@
 from feedgen.feed import FeedGenerator
 fg = FeedGenerator()
-fg.id('http://lernfunk.de/media/654321')
-fg.title('Some Testfeed')
-fg.author( {'name':'John Doe','email':'john@example.de'} )
-fg.link( href='http://example.com', rel='alternate' )
+fg.id('https://snow-services.com')
+fg.title('kTag Personal Nostr Feed')
+fg.author( {'name':'kTag','email':'sdf@sdf.com'} )
+fg.link( href='https://github.com/kTag/project-2503', rel='alternate' )
 fg.logo('http://ex.com/logo.jpg')
-fg.subtitle('This is a cool feed!')
-fg.link( href='http://larskiesow.de/test.atom', rel='self' )
+fg.subtitle('Nostr Feed')
+fg.link( href='https://snow-services.com', rel='self' )
 fg.language('en')
 
 fe = fg.add_entry()
